a1c0fb0f8db995874b479381d9f8d269e073d88f
Chris <chris@spendabit.co>
2019-02-25 18:05:30 -0500

    Add Gemfile(.lock) and a simple "test".

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+group :development, :test, :cucumber do
+  gem 'rspec'
+  # gem "rspec-core"
+end
+
---
+++ spec/tinker_spec.rb
@@ -0,0 +1,13 @@
+
+describe 'tinkering' do
+
+  it 'should pass tests that are legit' do
+    x = 3
+    expect(x * 3).to eq 9
+  end
+
+  # it 'fails' do
+  #   expect(true).to be false
+  # end
+
+end
