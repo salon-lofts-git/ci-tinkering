f10048313eecd01b5a1527768e9701e4daa86f8a
Chris <chris@spendabit.co>
2019-02-27 13:39:39 -0500

    spec/tinker_spec.rb: Add another dummy test...

--- spec/tinker_spec.rb
+++ spec/tinker_spec.rb
@@ -6,6 +6,10 @@ describe 'tinkering' do
     expect(x * 3).to eq 9
   end
 
+  it 'should also pass this fake test' do
+    expect(1 + 1).to eq 2
+  end
+
   # it 'fails' do
   #   expect(true).to be false
   # end
